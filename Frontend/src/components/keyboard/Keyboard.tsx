599eb15b74a1958e8fc17207fc5b2811bc984f8d
TheSilentIce <srikaryadlapati@gmail.com>
2025-10-17 20:32:05 -0700

    added .d.ts file for react piano

--- Frontend/src/components/keyboard/Keyboard.tsx
+++ Frontend/src/components/keyboard/Keyboard.tsx
@@ -1,14 +1,14 @@
-import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
-import 'react-piano/dist/styles.css';
-function Keyboard(){
-  const firstNote = MidiNumbers.fromNote('c3');
-  const lastNote = MidiNumbers.fromNote('f5');
+import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
+import "react-piano/dist/styles.css";
+function Keyboard() {
+  const firstNote = MidiNumbers.fromNote("c3");
+  const lastNote = MidiNumbers.fromNote("f5");
   const keyboardShortcuts = KeyboardShortcuts.create({
     firstNote: firstNote,
     lastNote: lastNote,
     keyboardConfig: KeyboardShortcuts.HOME_ROW,
   });
- 
+
   return (
     <Piano
       noteRange={{ first: firstNote, last: lastNote }}
